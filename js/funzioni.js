bb19f239567caf28498f11ad464696a3c2d8e18e
Capa99 <federicomanetta1999@gmail.com>
2018-11-19 14:19:41 +0100

    nemico che segue

--- js/funzioni.js
+++ js/funzioni.js
@@ -12,74 +12,132 @@ function generaBuconero(){
 function generaPortale(){
 	generaOggetto(PORTALE);
 }
+function Cacciatore (x,y,nome){
+this.x=x; 
+this.y=y;
+this.nome=nome;
+}
+	n1 = new Cacciatore(4,2,NEMICO);
+	setInterval("n1.insegui()", 800);
+	
 
-function generaNemico(){
-	n1 = new Nemico(3,3,NEMICO);
-	setInterval(n1.insegui,500);
+
+function calcolaNuovaPosizioneCacciatore(){
+	if(this.x > ominoX) {
+		this.x ++;
+	} else {
+		this.x--;
+	}
+	if(this.y > ominoY) {
+		this.y ++;
+	} else {
+		this.y--;
+	}
 }
 
+
+
+
+	Cacciatore.prototype.insegui = function () {
+	
+	if(Math.sqrt(Math.pow((ominoX-this.x),2)+Math.pow((ominoY-this.y),2))<5){	
+		var precX = this.x;
+		var precY = this.y;
+		var newX = this.x; 
+		var newY = this.y; 
+		if (this.x < ominoX) { newX = this.x +1; }
+		if (this.x > ominoX) { newX = this.x - 1; }
+		if (this.y < ominoY) {newY = this.y +1;}
+		if (this.y > ominoY) {newY = this.y - 1;}
+		
+		// adesso prima di spostare controlliamo che non ci sia un ostacolo nella cella: 
+		if (piano[newX][newY]!=OSTACOLO){
+		// assume la nuova posizione
+		this.x = newX; 
+		this.y = newY;
+		// si disegna il cacciatore nella nuova cella
+		document.getElementById("c"+precX+"_"+precY).src=pathImg+piano[precX][precY] + ".jpg";
+		document.getElementById("c"+this.x+"_"+this.y).src= pathImg+this.nome + ".jpg"; 
+	}
+	
+}	
+	else{
+		
+		var precX = this.x;
+		var precY = this.y;
+		var newX = this.x; 
+		var newY = this.y; 
+		var min=1; 
+        var max=4;  
+        var random =Math.floor(Math.random() * (+max - +min)) + +min;
+		if (random==1)
+			newX=(newX -1 + R)%R;
+		if (random==2)
+			newX=(newX +1 + R)%R;
+		if (random==3)
+			newY=(newY -1 + C)%C;
+		if (random==4)
+			newX=(newY +1 + C)%C;
+		
+		if (piano[newX][newY]!=OSTACOLO){
+		this.x= newX;
+		this.y= newY;
+		
+		document.getElementById("c"+precX+"_"+precY).src=pathImg+piano[precX][precY] + ".jpg";
+		document.getElementById("c"+this.x+"_"+this.y).src= pathImg+this.nome + ".jpg"; 
+			}}
+	}
+
+	
+	
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 function gameOver(){
 			if (energia<0){
             document.getElementById("en").innerHTML="HAI PERSO!";
 				document.getElementById("gioco").innerHTML="<img src=\"gameover.jpg\" >";
 				document.getElementById("energia").style.display="none";
 			}
-}
-function spostaNemico (){
-	if (controllaCellaNemico(ANX, ANY)){
-		var daSrc = "c" +nemicox+"_"+nemicoy; 
-	    var aSrc  = "c" + ANX+"_"+ ANY;
-		console.log(daSrc + " " +aSrc);
+}// il this.nome coincide con il nome dell’immagine nel file system 
 
-		document.getElementById(daSrc).src = pathImg +  piano[nemicox][nemicoy] + ".jpg";
-		nemicox= ANX;
-		nemicoy= ANY;
-		
-		disegnaCellaSpeciale(nemicox,nemicoy,NEMICO);
-	}
-	var min=1; 
-        var max=4;  
-        var random =Math.floor(Math.random() * (+max - +min)) + +min;
-		if (random==1)
-			ANX=(ANX -1 + R)%R;
-		if (random==2)
-			ANX=(ANX +1 + R)%R;
-		if (random==3)
-			ANY=(ANY -1 + C)%C;
-		if (random==4)
-			ANY=(ANY +1 + C)%C;
-}
-function controllaCellaNemico(x,y){
-var precX = this.x;
-	var precY = this.y;
 
-var newX = this.x; 
-var newY = this.y; 
 
-if (this.x < ominoX) { newX = this.x +1; }
-if (this.x > ominoX) { newX = this.x - 1; }
 
-if (this.y < ominoY) {newY = this.y +1;}
- if (this.y > ominoY) {newY = this.y - 1;}
-
-// adesso prima di spostare controlliamo che non ci sia un ostacolo nella cella: 
-
-if (piano[newX][newY]!=OSTACOLO){
-// assume la nuova posizione
-this.x = newX; 
-this.y = newY;
-// si disegna il cacciatore nella nuova cella
-document.getElementById(“c”+precX+”_“+precY).src   = piano[precX][precY ] + “.jpg”;
-document.getElementById(“c”+this.x+”_“+this.y).src    = this.nome + “.jpg”; 
-}
-
-// il this.nome coincide con il nome dell’immagine nel file system 
-
-if (this.x == ominoX && this.y == ominoY ){
-    gameOver();
-}
-
-}
 
 
 
