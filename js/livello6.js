11b34a8e543443ac1fc116ded2d60c0bade93b33
Stefachy <chincoli.stefano@gmail.com>
2018-11-22 15:58:03 +0100

    fixlivello6

--- js/livello6.js
+++ js/livello6.js
@@ -23,7 +23,7 @@ function livello6(){
 	piano[0][12]=OSTACOLO;
 	piano[4][12]=OSTACOLO;
 	piano[5][12]=OSTACOLO;
-	piano[15][13]=OSTACOLO;
+	piano[5][13]=OSTACOLO;
 	piano[5][14]=OSTACOLO;
 	piano[5][16]=OSTACOLO;
 	piano[5][17]=OSTACOLO;
@@ -45,11 +45,15 @@ function livello6(){
 	piano[0][17]=BUCONERO;
 	piano[0][19]=BUCONERO;
 	piano[2][4]=PORTACHIUSA;
-	piano[2][4]=PORTACHIUSA;
-	piano[2][4]=PORTACHIUSA;
+	piano[5][9]=PORTACHIUSA;
+	piano[2][8]=PORTACHIUSA;
 	piano[5][15]=PORTACHIUSA;
 	piano[1][12]=LASERONEGIU;
 	piano[3][12]=LASERONESU;
+	piano[9][18]=CHIAVE;
+	piano[0][10]=ARMA;
+	
+	
 	
 	i1=setInterval("raggioLaser(2,12)", 1500);
 	countPillole=4;
@@ -57,8 +61,8 @@ function livello6(){
 	n1=new Cacciatore(0,6,NEMICO);
 	intervallo1=setInterval("n1.insegui()",800);	
 	n2=new Cacciatore(2,18,NEMICO);
-	intervallo2=setInterval("n1.insegui()",800);	
+	intervallo2=setInterval("n2.insegui()",800);	
 	n3=new Cacciatore(6,19,NEMICO);
-	intervallo3=setInterval("n1.insegui()",800);
+	intervallo3=setInterval("n3.insegui()",800);
 }
 	
